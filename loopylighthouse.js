6db3e3ca8ffc317b116ee161da136aafbce9f670
Anthony Shadowitz <anthony.shadowitz@gmail.com>
2019-08-31 18:17:41 +0000

    loopylighthouse challenge

---
+++ loopylighthouse.js
@@ -0,0 +1,25 @@
+var x = 100;
+while (x >= 100 && x <= 200) {
+  if ((x % 3 === 0) && (x % 4 === 0)){
+        console.log("LoopyLighthouse");
+  }else if (x % 3 === 0){
+        console.log("Loopy");
+  }else if (x % 4 === 0){
+        console.log("Lighthouse");
+  }else {
+        console.log(x); 
+  }
+  x ++
+}
+
+for (var x =100; x <= 200; x++){
+  if ((x % 3 === 0) && (x % 4 === 0)){
+    console.log("LoopyLighthouse");
+  }else if (x % 3 === 0){
+    console.log("Loopy");
+  }else if (x % 4 === 0){
+    console.log("Lighthouse");
+  }else {
+    console.log(x); 
+  }
+}
