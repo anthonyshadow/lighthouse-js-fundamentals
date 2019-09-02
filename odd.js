258351db6614f373f026033fc45e7b9bf4df668b
Anthony Shadowitz <anthony.shadowitz@gmail.com>
2019-09-02 18:25:21 +0000

    odd calculator

---
+++ odd.js
@@ -0,0 +1,9 @@
+function isOdd(number) {
+  if (number % 2 === 0) {
+      console.log("even");
+  } else (number % 2 !== 0) {
+      console.log("odd");
+}
+
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
