ab3f478f87418c899e6e6398b224494cf3d9391e
Anthony Shadowitz <anthony.shadowitz@gmail.com>
2019-09-03 17:56:01 +0000

    #2

--- how-many-hundreds.js
+++ how-many-hundreds.js
@@ -1,9 +1,8 @@
-function howManyHundreds(num) {
-  return num % 100;
+function howManyHundreds(number)
+{
+   var result = (number - (number % 100))/100;
+   return result
 }
 
-console.log(howManyHundreds(1000), "=?", 10);
-console.log(howManyHundreds(894), "=?", 8);
-console.log(howManyHundreds(520), "=?", 5);
-console.log(howManyHundreds(99), "=?", 0);
-console.log(howManyHundreds(0), "=?", 0);
+
+ 
